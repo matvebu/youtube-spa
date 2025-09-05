c944a121b2561ac2ab63d03432313378ddd6a172
NastaVoronova <buchneva.anastasya92@gmail.com>
2025-09-05 18:23:33 +0300

    fix form

--- src/features/auth/ui/RegisterForm.tsx
+++ src/features/auth/ui/RegisterForm.tsx
@@ -4,6 +4,7 @@ import { AuthSchema, type AuthSchemaType } from '../schema/schema';
 import { Button } from '../../../components/ui/button';
 import { TextInputField } from './fields/TextInputField';
 import { NumericInputField } from './fields/NumericInputField';
+import { Form } from '../../../components/ui/form';
 
 const RegisterForm = () => {
   const form = useForm<AuthSchemaType>({
@@ -16,16 +17,18 @@ const RegisterForm = () => {
 
   return (
     <div className='w-[550px] mx-auto'>
-      <form className='grid gap-4 w-full max-w-md min-w-0' onSubmit={form.handleSubmit(onSubmit)}>
-        <TextInputField form={form} name='username' placeholder='username' label='Name' />
-        <TextInputField form={form} name='email' placeholder='email' label='E-mail' />
-        <TextInputField form={form} name='password' placeholder='password' label='Password' />
-        <TextInputField form={form} name='gender' placeholder='gender' label='Gender' />
-        <NumericInputField form={form} name='age' placeholder='age' label='Age' />
-        <Button className='mt-2 w-25' type='submit'>
-          register
-        </Button>
-      </form>
+      <Form {...form}>
+        <form className='grid gap-4 w-full max-w-md min-w-0' onSubmit={form.handleSubmit(onSubmit)}>
+          <TextInputField form={form} name='username' placeholder='username' label='Name' />
+          <TextInputField form={form} name='email' placeholder='email' label='E-mail' />
+          <TextInputField form={form} name='password' placeholder='password' label='Password' />
+          <TextInputField form={form} name='gender' placeholder='gender' label='Gender' />
+          <NumericInputField form={form} name='age' placeholder='age' label='Age' />
+          <Button className='mt-2 w-25' type='submit'>
+            register
+          </Button>
+        </form>
+      </Form>
     </div>
   );
 };
